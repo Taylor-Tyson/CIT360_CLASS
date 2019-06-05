1c49e567e67b3d9b11013c0f13a953a0d10097d1
tyson <taylort82@gmail.com>
2019-06-05 00:07:58 -0600

    Updated jUnit and added more testing and evaluating.

--- CIT360_CLASS/src/jUnit/jUnit.java
+++ CIT360_CLASS/src/jUnit/jUnit.java
@@ -2,23 +2,93 @@ package jUnit;
 
 public class jUnit {
 	
-	int a;
-	int b;
+	static int a;
+	static int b;
 	
-	public static int sum (int numA, int numB) {
-		return numA + numB;
+	/****************************************************
+	 * Method : sum
+	 ****************************************************/
+	public static int sum (String numA, String numB) {
+		if (checkInt(numA) && checkInt(numB) == true) {
+			a = Integer.parseInt(numA);  
+			b = Integer.parseInt(numB);
+			return a + b;
+		}
+		else {
+			return 0;
+		}	
 	}
 	
-	public static int sub (int numA, int numB) {
-		return numA - numB;
+	/****************************************************
+	 * Method : sub
+	 ****************************************************/
+	public static int sub (String numA, String numB) {
+		if (checkInt(numA) && checkInt(numB) == true) {
+			a = Integer.parseInt(numA);  
+			b = Integer.parseInt(numB);
+			return a - b;
+		}
+		else {
+			return 0;
+		}	
 	}
 	
-	public static int multiply (int numA, int numB) {
-		return numA * numB;
+	/****************************************************
+	 * Method : multiply
+	 ****************************************************/
+	public static int multiply (String numA, String numB) {
+		if (checkInt(numA) && checkInt(numB) == true) {
+			a = Integer.parseInt(numA);  
+			b = Integer.parseInt(numB);
+			return a * b;
+		}
+		else {
+			return 0;
+		}		
 	}
 	
-	public static int divide (int numA, int numB) {
-		return numA / numB;
+	/****************************************************
+	 * Method : divide
+	 ****************************************************/	
+	public static int divide (String numA, String numB) {
+		
+		if (checkInt(numA) && checkInt(numB) == true){
+			a = Integer.parseInt(numA);  
+			b = Integer.parseInt(numB);
+			
+			if (divideByZero(b)== true){
+				return a/b;
+			}
+		}
+		return 0;
 	}
-
+			
+	/****************************************************
+	 * Method : divideByZero
+	 ****************************************************/
+	public static boolean divideByZero (int numA) {
+		try 
+		{
+			if (numA == 0)
+				throw new ArithmeticException();
+			else 
+			 return true;	
+		}
+		catch (ArithmeticException e) {
+			return false;
+		}
+	}
+	
+	/****************************************************
+	 * Method : checkInt
+	 ****************************************************/	
+	public static boolean checkInt (String numA) {
+		try {
+			Integer.parseInt(numA);
+		}
+		catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
 }
--- CIT360_CLASS/src/jUnit/jUnitTest.java
+++ CIT360_CLASS/src/jUnit/jUnitTest.java
@@ -1,32 +1,144 @@
 package jUnit;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class jUnitTest {
-     
+    
+	//Test sum
     @Test
     public void testSum() {
-    	int val = jUnit.sum(7,5);
+    	int val = jUnit.sum("7","5");
         assertEquals(val, 12);
-    }
-	
-    @Test
-    public void testSub() {
-    	int val = jUnit.sub(7,5);
-        assertEquals(val,2);
     }
     
     @Test
+    public void testSum1() {
+    	int val = jUnit.sum("A","5");
+        assertEquals(val, 0);
+    }
+    
+    @Test
+    public void testSum2() {
+    	int val = jUnit.sum("7","A");
+        assertEquals(val, 0);
+    }
+    
+    @Test
+    public void testSum3() {
+    	int val = jUnit.sum("A","B");
+        assertEquals(val, 0);
+    }
+	
+    
+    //Test sub
+    @Test
+    public void testSub() {
+    	int val = jUnit.sub("7","5");
+        assertEquals(val,2);
+    }
+    
+    public void testSub1() {
+    	int val = jUnit.sub("A","5");
+        assertEquals(val,0);
+    }
+    
+    public void testSub2() {
+    	int val = jUnit.sub("7","A");
+        assertEquals(val,0);
+    }
+    
+    public void testSub3() {
+    	int val = jUnit.sub("A","B");
+        assertEquals(val,0);
+    }
+        
+    
+    //Test multiply
+    @Test
     public void testMultiply() {
-    	int val = jUnit.multiply(5,5);
+    	int val = jUnit.multiply("5","5");
         assertEquals(val,25);
     }
     
     @Test
+    public void testMultiply1() {
+    	int val = jUnit.multiply("A","5");
+    	assertEquals(val,0);
+    }
+    
+    @Test
+    public void testMultiply2() {
+    	int val = jUnit.multiply("5","B");
+    	assertEquals(val,0);
+    }
+    
+    @Test
+    public void testMultiply3() {
+    	int val = jUnit.multiply("A","B");
+    	assertEquals(val,0);
+    }
+        
+    
+    //Test divideByZero
+    @Test
+    public void testDivideByZeroTrue() {
+    	boolean val = jUnit.divideByZero(1);
+        assertTrue(val);
+    }
+    
+    @Test
+    public void testDivideByZeroFalse() {
+    	boolean val = jUnit.divideByZero(0);
+        assertFalse(val);
+    }
+    
+    
+    //Test Divide
+    @Test
+    public void testDivideZero() {
+    	int val = jUnit.divide("6","0");
+        assertEquals(val,0);
+    }
+    
+    @Test
     public void testDivide() {
-    	int val = jUnit.divide(6,2);
+    	int val = jUnit.divide("6","2");
         assertEquals(val,3);
+    }
+         
+    @Test
+    public void testDivide1() {
+    	int val = jUnit.divide("A","1");
+        assertEquals(val,0);
+    }
+    
+    @Test
+    public void testDivide2() {
+    	int val = jUnit.divide("6","A");
+        assertEquals(val,0);
+    }
+	
+    @Test
+    public void testDivide3() {
+    	int val = jUnit.divide("B","A");
+        assertEquals(val,0);
+    }
+    
+    
+    //Test checkInt
+    @Test
+    public void testCheckIntTrue() {
+    	boolean val = jUnit.checkInt("6");
+        assertTrue(val);
+    }
+    
+    @Test
+    public void testCheckIntFalse() {
+    	boolean val = jUnit.checkInt("a");
+        assertFalse(val);
     }
 	
 }
